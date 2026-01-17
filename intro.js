5ac901f6d99ac2fd04120b79e4fcfc3b1d8b01d6
DeJosh-stack <babujoshua855@gmail.com>
2026-01-17 12:36:03 +0000

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,7 @@
+const myname = "Michael Mboya";
+const myage = 20;
+const favlanguage = "Python";
+
+console.log("Name:" + myname);
+console.log("myage:" + myage);
+console.log("favlanguage:" + favlanguage);
